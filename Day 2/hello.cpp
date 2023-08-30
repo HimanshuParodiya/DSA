dda55da5f46bf5d64fb09bd0995974a3d110572c
Himanshu <himanshuparodiya@gmail.com>
2023-08-30 15:46:45 +0530

    getting start with c++

---
+++ Day 2/hello.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    // cout<<"Namaste Duniya"<<endl;
+    // double d = 1.23;
+    // float f = 123;
+    //  char me = 'h';
+    //  string a = "hello" ;
+    // cout<< sizeof(string)<<endl;
+    // cout<<f<< d<<endl;
+    // int a = 2/5; 
+    // cout<<a; // 0
+    // cout<<2.0/5<<endl;
+
+    // float b = 2.0/5;
+    // // float b = 2/5; // it will not work because int/int
+    // cout<<sizeof(b)<<endl; // 0.4 // size 4
+
+    // double c = 2.0/5;
+    // cout<<sizeof(c)<<endl; // 0.4 // size 8
+
+    int a = 3 << 3 ;
+    cout<<a<<endl; // 6
+} 
