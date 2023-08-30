f5ba6a5f32fe12c34914208acf0411dce3ba081c
Himanshu <himanshuparodiya@gmail.com>
2023-08-30 15:55:18 +0530

    If else and while loop

---
+++ Day 3 If else loop/if_else.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 6;
+
+    // if (a>0)
+    // {
+    //     cout<<"+ve value"<<endl; // output
+    // }
+    // else
+    // {
+    //     cout<<"-ve value"<<endl;
+    // }
+
+    // int b;
+    // b = cin.get();
+
+    // cout<<"value of b is "<<b<<endl; // if 1 then 49 According to ASCII
+
+    // int a = 2;
+    // int b = a + 1;
+
+    // if((a=3)==b){
+    //     cout<<a;
+    // }else{
+    //     cout<<a+1<<"A+1";
+    // }
+
+    int a = 24;
+
+    if (a > 20)
+    {
+        cout << "First";
+    }
+    else if (a == 24)
+    {
+        cout << "Second";
+    }
+    else
+    {
+        cout << "third";
+    };
+
+    char b = cin.get();
+    cout << "b is " << b << endl;
+
+    cout
+        << a;
+
+    return 0;
+}
