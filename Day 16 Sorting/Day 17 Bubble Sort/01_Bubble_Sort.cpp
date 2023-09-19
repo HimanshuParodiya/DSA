f4c58361a2b95ffa21a9c09ff1af3dfaa399ff26
Himanshu <himanshuparodiya@gmail.com>
2023-09-19 21:32:15 +0530

    Implement bubble sort for best case

--- Day 16 Sorting/Day 17 Bubble Sort/01_Bubble_Sort.cpp
+++ Day 16 Sorting/Day 17 Bubble Sort/01_Bubble_Sort.cpp
@@ -5,20 +5,36 @@ using namespace std;
 int main()
 {
     vector<int> arr = {6, 2, 8, 4, 10};
-    for (int i = 1; i < arr.size(); i++) //
+    // vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    int n = arr.size();
+
+    // is already Sorted
+    bool isSorted = false;
+
+    for (int i = 1; i < n; i++) // controls the number of passes.
     {
-        for (int j = 0; j < arr.size() - i; j++) // i = 1 so loop will go to n-1 next time last element is biggest so dont need to compare with it so next time i = 2 so loop will go from 0 to n-2
+        for (int j = 0; j < n - i; j++) // When i = 1, the inner loop goes up to n - 1. In the next iteration, the largest element is already in its correct position, so there's no need to compare it. When i = 2, the inner loop goes from 0 to n - 2
         {
-            if (arr[j] > arr[j + 1]) // check for next element if current element is grater than next element then swap it
+            if (arr[j] > arr[j + 1]) // compare current element with the next element.
             {
-                swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j + 1]); // Swap them if the current element is greater.
+                isSorted = true;          // for 1st round if any element swapped that mean array is not sorted then the time complexity will be O(n^2)
             }
+        }
+
+        if (!isSorted) // if this runs in first time then this is best case with time complexity of O(n)
+        {
+            cout << "Already Sorted ";
+            break;
         }
     }
 
-    for (int i = 0; i < arr.size(); i++) //  Print the sorted elements
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++) // Print the sorted elements
     {
-        cout << arr[i];
+        cout << arr[i] << " ";
     }
 
     return 0;
