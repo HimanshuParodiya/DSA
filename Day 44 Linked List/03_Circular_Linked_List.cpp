4041d0c99adfb2a39e7b4a9e8fadb083b237c2c9
Himanshu <himanshuparodiya@gmail.com>
2023-11-26 20:32:43 +0530

    Deleting single node when there is 1 node in linked list

--- Day 44 Linked List/03_Circular_Linked_List.cpp
+++ Day 44 Linked List/03_Circular_Linked_List.cpp
@@ -43,13 +43,12 @@ public:
 
 void printLinkedList(Node *tail)
 {
+    Node *temp = tail->next;
     if (tail == NULL)
     {
         cout << "List is empty" << endl;
         return;
     }
-
-    Node *temp = tail->next;
 
     do
     {
@@ -114,6 +113,13 @@ void deleteNode(Node *&tail, int value)
         }
 
         prev->next = current->next;
+        // deleting node from 1 node in linked list
+        if (current == prev) // means there is only 1 node in linked list
+        {
+            tail = NULL;
+        }
+
+        // deleting node from more than 2 node in linked list
         if (tail == current)
         {
             tail = prev;
@@ -130,19 +136,31 @@ int main()
     // Node *tail = node1;
     // printLinkedList(tail);
 
+    // Node *tail = NULL;
+    // insertNode(tail, 5, 3);
+    // printLinkedList(tail);
+    // insertNode(tail, 3, 6); // search for 3 in linked list and insert 6 there
+    // printLinkedList(tail);
+    // insertNode(tail, 6, 9); // search for 6 in linked list and insert 9 there
+    // printLinkedList(tail);
+    // insertNode(tail, 9, 1); // search for 6 in linked list and insert 1 there
+    // printLinkedList(tail);
+    // insertNode(tail, 3, 4); // search for 6 in linked list and insert 1 there
+    // printLinkedList(tail);
+
+    // deleteNode(tail, 3);
+    // printLinkedList(tail);
+
+    // now we if linked list consist 1 node so we are trying to delete it.
     Node *tail = NULL;
+
     insertNode(tail, 5, 3);
     printLinkedList(tail);
-    insertNode(tail, 3, 6); // search for 3 in linked list and insert 6 there
-    printLinkedList(tail);
-    insertNode(tail, 6, 9); // search for 6 in linked list and insert 9 there
-    printLinkedList(tail);
-    insertNode(tail, 9, 1); // search for 6 in linked list and insert 1 there
-    printLinkedList(tail);
-    insertNode(tail, 3, 4); // search for 6 in linked list and insert 1 there
+
+    deleteNode(tail, 3);
     printLinkedList(tail);
 
-    deleteNode(tail, 6);
-    printLinkedList(tail);
+    // when we are deleting single node we are getting error because tail, prev and current everything pointing on that single node and then we are deleting it then we are printing it using tail which is raising an error of segmentation fault
+    // so now we will create a case for deleting node when there is only 1 node in linked list
     return 0;
 }
