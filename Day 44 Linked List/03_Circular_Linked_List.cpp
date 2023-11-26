17b0e3b613c21986e65072f1bc834710f59a8bce
Himanshu <himanshuparodiya@gmail.com>
2023-11-26 20:15:14 +0530

    Delete Node by value

--- Day 44 Linked List/03_Circular_Linked_List.cpp
+++ Day 44 Linked List/03_Circular_Linked_List.cpp
@@ -28,17 +28,36 @@ public:
     }
 };
 
+// void printLinkedList(Node *tail)
+// {
+//     Node *temp = tail->next; // storing address of tail
+
+//     do
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next; // going to next node and we are taking tail as by value so we can update tail
+//     } while (tail->next != temp);
+
+//     // cout << endl;
+// }
+
 void printLinkedList(Node *tail)
 {
-    Node *temp = tail; // storing address of tail
+    if (tail == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    Node *temp = tail->next;
 
     do
     {
         cout << temp->data << " ";
-        temp = temp->next; // going to next node and we are taking tail as by value so we can update tail
-    } while (tail->next != temp);
+        temp = temp->next;
+    } while (temp != tail->next);
 
-    // cout << endl;
+    cout << endl;
 }
 
 void insertNode(Node *&tail, int element, int data)
@@ -71,6 +90,40 @@ void insertNode(Node *&tail, int element, int data)
     }
 };
 
+void deleteNode(Node *&tail, int value)
+{
+    // empty list
+
+    if (tail == NULL)
+    {
+        cout << "List is empty, please check again" << endl;
+        return;
+    }
+    else
+    {
+        // non-empty
+
+        // assuming that "value" is present in the linked list
+        Node *prev = tail;
+        Node *current = prev->next;
+
+        while (current->data != value)
+        {
+            prev = current;
+            current = current->next;
+        }
+
+        prev->next = current->next;
+        if (tail == current)
+        {
+            tail = prev;
+        }
+
+        current->next = NULL;
+        delete current;
+    }
+}
+
 int main()
 {
     // Node *node1 = new Node(5);
@@ -85,6 +138,11 @@ int main()
     insertNode(tail, 6, 9); // search for 6 in linked list and insert 9 there
     printLinkedList(tail);
     insertNode(tail, 9, 1); // search for 6 in linked list and insert 1 there
+    printLinkedList(tail);
+    insertNode(tail, 3, 4); // search for 6 in linked list and insert 1 there
+    printLinkedList(tail);
+
+    deleteNode(tail, 6);
     printLinkedList(tail);
     return 0;
 }
