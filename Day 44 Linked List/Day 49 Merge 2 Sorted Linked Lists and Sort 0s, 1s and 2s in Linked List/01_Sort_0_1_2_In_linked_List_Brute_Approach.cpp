e5c6d6436da8ce67f694087f9e381552bc42b37f
Himanshu <himanshuparodiya@gmail.com>
2023-12-05 21:17:39 +0530

    Sort 0s, 1s and 2s in the linked list Brute Force Approach

---
+++ Day 44 Linked List/Day 49 Merge 2 Sorted Linked Lists and Sort 0s, 1s and 2s in Linked List/01_Sort_0_1_2_In_linked_List_Brute_Approach.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1)
+    {
+        data = data1;
+        next = NULL;
+    }
+};
+
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]); // pointing head to 1st node
+    Node *mover = head;            // mover pointing to 1st node
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+void printLinkedList(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node *sortList(Node *&head)
+{
+    int zeroCount = 0;
+    int oneCount = 0;
+    int twoCount = 0;
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+        {
+            zeroCount++;
+        }
+        else if (temp->data == 1)
+        {
+            oneCount++;
+        }
+        else if (temp->data == 2)
+        {
+            twoCount++;
+        }
+        temp = temp->next;
+    }
+
+    temp = head;
+    while (temp != NULL)
+    {
+        if (zeroCount != 0)
+        {
+            temp->data = 0;
+            zeroCount--;
+        }
+        else if (oneCount != 0)
+        {
+            temp->data = 1;
+            oneCount--;
+        }
+        else if (twoCount != 0)
+        {
+            temp->data = 2;
+            twoCount--;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {0, 1, 2, 1, 2};
+    Node *head = convertArr2LL(arr);
+    printLinkedList(head);
+    sortList(head);
+    printLinkedList(head);
+    return 0;
+}
